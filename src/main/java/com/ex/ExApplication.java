c6916e60fc98bb93d2cbfde286c4125119dc4057
Giulio <giulio.lpdenardi@gmail.com>
2016-03-18 07:30:20 -0300

    Initial Commit

---
+++ src/main/java/com/ex/ExApplication.java
@@ -0,0 +1,12 @@
+package com.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExApplication.class, args);
+	}
+}
