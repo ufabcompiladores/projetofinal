ff135c37bbc0c44ebf6133e15fffbd5b1a6284d2
Andre0991 <andre.peric.tavares@gmail.com>
2016-05-04 21:08:28 -0300

    Atualiza pdf e html

--- relatorio/auto/relatorio.el
+++ relatorio/auto/relatorio.el
@@ -5,12 +5,12 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("babel" "portuguese" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -48,11 +48,12 @@
     "sec:orgheadline15"
     "sec:orgheadline16"
     "sec:orgheadline17"
-    "sec:orgheadline3"
     "sec:orgheadline18"
+    "sec:orgheadline3"
     "sec:orgheadline19"
     "sec:orgheadline20"
     "sec:orgheadline21"
-    "sec:orgheadline22"))
+    "sec:orgheadline22"
+    "sec:orgheadline23"))
  :latex)
 
